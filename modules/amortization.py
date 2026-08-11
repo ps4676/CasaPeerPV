fbcf016da70e92dabb75785ba46f7df70dca38e9
Samuel Peer <ps4676@mci4me.at>
2026-08-11 15:41:53 +0200

    amortisation überschrift

--- modules/amortization.py
+++ modules/amortization.py
@@ -24,10 +24,9 @@ def show_amortization(werte):
     # ========================================================
     # KENNZAHL
     # ========================================================
-    letztes_datum = t.max()
-    st.subheader(f"Gesamtwerte bis {letztes_datum.strftime('%d.%m.%Y')}")
-    
-    st.metric(f"{amortisation:.2f} %")
+    st.subheader(f"Stand am {t.max().strftime('%d.%m.%Y')}:")
+
+    st.metric(f"Amortisation","{amortisation:.2f} %")
 
 
     # ========================================================
